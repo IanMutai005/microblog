d7d448f8ad8d11abe422664075e77abfc581c69b
Ian <iankipmutai005@gmail.com>
2026-03-23 17:28:29 +0300

    Templates : step 4 rendering our html code

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World !"
+
+    user = {'username' : 'Martin'}
+    posts = [
+        {
+            'author' : {'username':'Ian'},
+            'body' : 'Beautiful day in Nairobi'
+        },
+        {
+            'author' : {'username':'Mutai'},
+            'body' : 'Arsenal is the greatest team ever'
+        }
+    ]
+    return render_template('index.html',title = 'Home', user = user , posts =posts)
