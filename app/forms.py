10744161ee7b727b3d8ec170ad795e5877753b2a
Ian <iankipmutai005@gmail.com>
2026-03-26 22:56:20 +0300

    Web forms 5 updating our base and the other relevant modules

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField , PasswordField , BooleanField , SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField("username" , validators= [DataRequired()])
-    password = StringField("Password" , validators= [DataRequired()])
-    remember_me = StringField("Remember me" )
-    submit = StringField("Sign in " )
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
