abddd39ecd0a82a91c62842b95becb5c05441047
Ian <iankipmutai005@gmail.com>
2026-04-06 21:13:46 +0300

    Database 3: creating our base user model

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+
+
+class User(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), unique=True, index=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), unique=True, index=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(128))
+
+    def __repr__(self):
+        return '<User{}>'.format(self.username)
