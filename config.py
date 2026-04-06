7100fa1b631491e4c50dd52d4720fd22fcedd515
Ian <iankipmutai005@gmail.com>
2026-04-06 21:20:25 +0300

    Database 4 : updated config file

--- config.py
+++ config.py
@@ -4,5 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config :
    
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Mamba-one'
-    SQLALCHEMY_DATABASE_URI = os.eniron.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir,'app.db')
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir,'app.db')
