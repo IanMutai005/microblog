38b5700f3c669b0a3b767e0dfd37511a766c3231
Ian <iankipmutai005@gmail.com>
2026-04-06 21:02:04 +0300

    Database 1 : updating our config file

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config :
    
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Mamba-one'
+    SQLALCHEMY_DATABASE_URI = os.eniron.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir,'app.db')
+    
